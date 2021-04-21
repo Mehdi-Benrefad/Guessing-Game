b51365f97e1da56df0ab9676600a1602a67d80ff
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-04-21 17:26:55 +0000

    count

--- Devinette/ViewController.swift
+++ Devinette/ViewController.swift
@@ -17,40 +17,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var input: UITextField!
     var random=Int()
     var entree=Int()
+    var count=Int()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         random = Int(arc4random_uniform(10))
         // Do any additional setup after loading the view.
         input.backgroundColor = UIColor.white
-        //on teste l'etat de User Default
-            switch backgroundService.color {
-               case "black":
-                   view.backgroundColor = UIColor.black
-                   `switch`.isOn = true
-                    bouton.backgroundColor = UIColor.white
-                    bouton.titleLabel?.textColor = UIColor.black
-                    titre.textColor = UIColor.white
-                    label.textColor = UIColor.white
-                case "white":
-                   view.backgroundColor = UIColor.white
-                    `switch`.isOn = false
-                   bouton.backgroundColor = UIColor.black
-                   bouton.titleLabel?.textColor = UIColor.white
-                    titre.textColor = UIColor.black
-                    label.textColor = UIColor.black
-            default:
-                   break
-               }
+        //on initialise lemode et les couleurs
+            initialiserMode()
         }
 
     //on change la couleur de background et on modifie la variable dans user default
     @IBAction func changeMode(_ sender: UISwitch) {
+       changefromSwitch()
+    }
+    
+    @IBAction func jouer(_ sender: Any) {
+    
+       count = count + 1
+        
+        if Int(input.text!) == random {
+             let alertVC = UIAlertController(title: "Winner", message: "vous gagnez le jeu apres \(count) essai", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alertVC, animated: true, completion: nil)
+            
+        }
+        else if Int(input.text!)! < random {
+             let alertVC = UIAlertController(title: "petite", message: "la valeur est inferieur au random", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alertVC, animated: true, completion: nil)
+            
+        }else if Int(input.text!)! > random {
+             let alertVC = UIAlertController(title: "grande", message: "la valeur est superieur au random", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alertVC, animated: true, completion: nil)
+            
+        }
+        
+    }
+
+    
+    func changefromSwitch(){
         if `switch`.isOn == true{
             backgroundService.color = "black"
             view.backgroundColor = UIColor.black
-            bouton.backgroundColor = UIColor.white
-            bouton.titleLabel?.textColor = UIColor.black
+            bouton.backgroundColor = UIColor.gray
             titre.textColor = UIColor.white
             label.textColor = UIColor.white
         }else{
@@ -62,42 +74,28 @@ class ViewController: UIViewController {
             label.textColor = UIColor.black
             
         }
-    }
-    
-    @IBAction func jouer(_ sender: Any) {
-        
-        
-        
-        if Int(input.text!) == random {
-             let alertVC = UIAlertController(title: "Winner", message: "vous gagnez le jeu", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            present(alertVC, animated: true, completion: nil)
-            
-        }
-        /*else {
-            
-             let alertVC = UIAlertController(title: "Perdu", message: "vous avez perdu le jeu", preferredStyle: .alert)
-                alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                       present(alertVC, animated: true, completion: nil)
-            
-        }*/
-        if Int(input.text!)! < random {
-             let alertVC = UIAlertController(title: "petite", message: "la valeur est inferieur au random", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            present(alertVC, animated: true, completion: nil)
-            
-        }
-        
-        if Int(input.text!)! > random {
-             let alertVC = UIAlertController(title: "grande", message: "la valeur est superieur au random", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            present(alertVC, animated: true, completion: nil)
-            
-        }
         
     }
-
     
-    
+    func initialiserMode(){
+        switch backgroundService.color {
+           case "black":
+               view.backgroundColor = UIColor.black
+               `switch`.isOn = true
+                bouton.backgroundColor = UIColor.gray
+                titre.textColor = UIColor.white
+                label.textColor = UIColor.white
+            case "white":
+               view.backgroundColor = UIColor.white
+                `switch`.isOn = false
+               bouton.backgroundColor = UIColor.black
+               bouton.titleLabel?.textColor = UIColor.white
+                titre.textColor = UIColor.black
+                label.textColor = UIColor.black
+        default:
+               break
+           }
+        
+    }
 }
 
